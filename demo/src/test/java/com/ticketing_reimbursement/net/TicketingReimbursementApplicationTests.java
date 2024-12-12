ece374a5010029aa0618c516a78f600a01ea33d1
wadea1984 <wadea1984@gmail.com>
2024-12-12 14:16:56 -0500

    started Spring API

---
+++ demo/src/test/java/com/ticketing_reimbursement/net/TicketingReimbursementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketing_reimbursement.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingReimbursementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
